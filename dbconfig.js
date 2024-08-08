9fa6c6e1d3c1a5b24119bfff3a7e7e4598254eb9
jamyyu <jamy11212@gmail.com>
2024-08-08 20:30:45 +0800

    Add database configuration in dbconfig.js

---
+++ dbconfig.js
@@ -0,0 +1,46 @@
+import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+
+
+const pool = mysql.createPool({
+  host: dbHost,
+  user: dbUser,
+  password: dbPassword,
+  database: 'Splitmate',
+  waitForConnections: true,
+  connectionLimit: 20, 
+  queueLimit: 0 // 等待連線請求數，0 表示不限制
+});
+
+// 使用連接池查詢
+export class Database {
+  static async executeQuery(query, params = []) {
+    let connection;
+    try {
+      connection = await pool.getConnection();
+      const [results] = await connection.execute(query, params); //使用解構賦值後，results 會被賦值為第一個元素
+
+      // SELECT 查詢
+      if (query.trim().toLowerCase().startsWith("select")) {
+        return results;
+      } else {
+        // 非 SELECT 查詢
+        return {
+          affectedRows: results.affectedRows,
+          insertId: results.insertId
+        };
+      }
+    } catch (error) {
+      console.error('Error while connecting to MySQL', error);
+      throw error;
+    } finally {
+      if (connection) connection.release();
+    }
+  }
+}
