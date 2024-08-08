fdf840695575196433f51add5d58757a395efa27
jamyyu <jamy11212@gmail.com>
2024-08-09 00:33:33 +0800

    Add S3 service and image resizing functionality
    - Add S3.js for handling file uploads to Amazon S3
    - Add resizeImg.js for image resizing functionality

---
+++ services/resizeImg.js
@@ -0,0 +1,19 @@
+import sharp from 'sharp';
+
+export const resizeImage = async function (fileBuffer) {
+  try {
+    // 獲取圖像的數據（包括寬度）
+    const metadata = await sharp(fileBuffer).metadata();
+    // 如果圖像寬度小於700，不進行調整，直接返回原始buffer
+    if (metadata.width && metadata.width <= 700) {
+      return fileBuffer;
+    }
+    // 如果圖像寬度大於700，調整圖像大小
+    const buffer = await sharp(fileBuffer)
+      .resize({ width: 700, fit: 'contain' })
+      .toBuffer();
+    return buffer;
+  } catch (error) {
+    throw error;
+  }
+}
