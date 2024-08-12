ffdac3e11d9de92b46a67c059051436600063ddf
jamyyu <jamy11212@gmail.com>
2024-08-12 18:01:28 +0800

    Implement getGroupsData function to retrieve and format group data

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,10 +1,11 @@
 import 'dotenv/config';
 import { uploadFileToS3 } from '../services/S3.js';
-import { createGroup } from '../models/groupModel.js';
+import { createGroup, getGroup } from '../models/groupModel.js';
 import { createMember, getMemberByEmail } from '../models/memberModel.js';
 import { getUserByEmail } from '../models/userModel.js';
-import { addMemberToGroup } from '../models/groupMemberModel.js';
+import { addMemberToGroup, getGroupByUserId, getMemberByGroupId} from '../models/groupMemberModel.js';
 import validator from 'validator';
+import jwt from 'jsonwebtoken';
 
 
 // 將沒有原型的物件轉換為普通物件
@@ -21,7 +22,17 @@ function convertToPlainObject(obj) {
 }
 
 
+const secretKey = process.env.JWT_SECRET_KEY;
+
 export const uploadGroupData = async (req, res) => {
+  // 會員驗證
+  const token = req.headers.authorization?.split(' ')[1];
+  console.log(token);
+  try {
+    const payload = jwt.verify(token, secretKey, { algorithms: ['HS256'] });
+  } catch (error) {
+    return res.status(403).json({ error: true, message: 'Not logged in, access denied' });
+  }
   try {
     // 圖片上傳至S3
     let imageName = null;
@@ -43,7 +54,7 @@ export const uploadGroupData = async (req, res) => {
       }
     }
     // 資料存入群組資料庫
-    const groupResults = await createGroup(plainBody.groupName, plainBody.mainCurrency, imageName)
+    const groupResults = await createGroup(plainBody.groupName, plainBody.mainCurrency, imageName);
     let groupId = groupResults;
     console.log('groupId',groupId);
     // 資料存入成員資料庫
@@ -72,12 +83,49 @@ export const uploadGroupData = async (req, res) => {
     res.status(200).json({ ok: true });
   } catch (error) {
     console.error('Error uploading file:', error);
-    res.status(500).send('Error uploading file');
+    return res.status(500).send('Error uploading file');
   }
 };
 
-export const getGroup = async (req, res) => {
-  image_url = 'https://d3q4cpn0fxi6na.cloudfront.net/' + image_name;
-  
+
+export const getGroupsData = async (req, res) => {
+  // 會員驗證
+  const token = req.headers.authorization?.split(' ')[1];
+  let userId;
+  try {
+    const payload = jwt.verify(token, secretKey, { algorithms: ['HS256'] });
+    userId = payload.id;
+  } catch (error) {
+    return res.status(403).json({ error: true, message: 'Not logged in, access denied' });
+  }
+  // 以 userId 取得其所在 groupId 取得群組名稱跟圖片
+  try {
+    const groupIdResults = await getGroupByUserId(userId);
+    // 檢查是否有群組
+    if (groupIdResults === null) {
+      return res.status(200).json({ data: [], message: 'No groups found' });
+    }
+    const groupsData = await Promise.all(groupIdResults.map(async group => {
+      let groupId = group.splitgroup_id;
+      let groupData = await getGroup(groupId);
+      groupData = groupData[0];
+      //let memberIdResults = await getMemberByGroupId(groupId);
+      //let memberId = memberIdResults.map(member => member.member_id);
+      return groupData;
+    }));
+    console.log('結果',groupsData);
+    // image_name 改成 imageUrl
+    const updatedgroupsData = groupsData.map(data => {
+      const imageUrl = 'https://d3q4cpn0fxi6na.cloudfront.net/' + data.image_name;
+      return {
+        ...data,  // 保留其他字段
+          image_url: imageUrl // 添加新的 image_url 字段
+      };
+    });
+    console.log('結果',updatedgroupsData);
+    res.status(200).json({ data: updatedgroupsData });
+  } catch (error) {
+    return res.status(400).json({ error: true, message: error });
+  }
 }
 
