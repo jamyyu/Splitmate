01fb53c0e694ceb66ff82eda6f0781c5b5c92106
jamyyu <jamy11212@gmail.com>
2024-08-12 17:53:46 +0800

    Add PATCH route for updating user profile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import { signUp, signIn, checkAuth } from '../controllers/userController.js';
+import { signUp, signIn, checkAuth, patchUserProfile } from '../controllers/userController.js';
+import multer from 'multer';
 
 const router = express.Router();
+
+// 設置 multer 的存儲方式為內存存儲
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 router.post('/api/user', signUp);
 
 router.post('/api/user/auth', signIn);
 
 router.get('/api/user/auth', checkAuth);
+
+router.patch('/api/user',upload.single('profilePicture'), patchUserProfile)
 
 export default router;
