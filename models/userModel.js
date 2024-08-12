48038bfbd447f46cab6ea29a937fb78b9fc99393
jamyyu <jamy11212@gmail.com>
2024-08-12 17:57:04 +0800

    Add updateUserData function to update user's name and profile image in the database

--- models/userModel.js
+++ models/userModel.js
@@ -16,8 +16,28 @@ export const createUser = async (name, email, hashedPassword) => {
 };
 
 export const getUserByEmail = async (email) => {
-  const query = 'SELECT id, name, email, password FROM user WHERE email = ?';
+  const query = 'SELECT id, name, image_name, email, password FROM user WHERE email = ?';
   const results = await Database.executeQuery(query, [email]);
   console.log('getUserByEmail',results);
   return results.length ? results[0] : null; 
 };
+
+export const updateUserData = async (name, image_name, email) => {
+  const query = 'UPDATE user SET name = ?, image_name = ? WHERE email = ?';
+  const results = await Database.executeQuery(query, [name, image_name, email]);
+  console.log('updateUserData',results);
+  return results 
+};
+
+//export const updateUserData = async (name, image_name, id) => {
+  //const query = 'UPDATE user SET name = ?, image_name = ? WHERE id = ?';
+  
+  //try {
+    //const results = await Database.executeQuery(query, [name, image_name, id]);
+    //console.log('User data updated successfully:', results);
+    //return results;
+  //} catch (error) {
+    //console.error('Error updating user data:', error);
+    //throw new Error('Failed to update user data');
+  //}
+//};
