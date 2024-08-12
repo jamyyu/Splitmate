478dbd1ea0d62660c36972fb5b08c28cd5c77c9b
jamyyu <jamy11212@gmail.com>
2024-08-12 17:59:47 +0800

    Add database functions to handle group and member mapping

--- models/groupMemberModel.js
+++ models/groupMemberModel.js
@@ -7,3 +7,27 @@ export const addMemberToGroup = async (groupId, memberId, userId, role) => {
   console.log('addMemberToGroup', results);
   return results;
 };
+
+
+export const getGroupByUserId = async (userId) => {
+  const query = 'SELECT splitgroup_id FROM GroupMemberMapping WHERE user_id = ?';
+  const results = await Database.executeQuery(query, [userId]);
+  console.log('getGroupByUserId',results);
+  return results.length ? results : null; 
+};
+
+
+export const getMemberByGroupId = async (groupId) => {
+  const query = 'SELECT member_id FROM GroupMemberMapping WHERE splitgroup_id = ?';
+  const results = await Database.executeQuery(query, [groupId]);
+  console.log('getMemberByGroupId',results);
+  return results.length ? results : null; 
+}
+
+
+export const updateGroupMemberMappingUserId = async (userId, memberId) => {
+  const query = 'UPDATE GroupMemberMapping SET user_id = ? WHERE member_id = ?';
+  const results = await Database.executeQuery(query, [userId, memberId]);
+  console.log('updateGroupMemberMappingUserId',results);
+  return results;
+}
