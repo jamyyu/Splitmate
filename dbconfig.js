0d0ed5e4c596787b19e9f36ae3fa5fecdad5f559
jamyyu <jamy11212@gmail.com>
2024-08-16 14:57:41 +0800

    Add resetPool function

--- dbconfig.js
+++ dbconfig.js
@@ -7,42 +7,58 @@ const dbHost = process.env.DB_HOST;
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
 
+let pool = createPool();
 
-const pool = mysql.createPool({
-  host: dbHost,
-  user: dbUser,
-  password: dbPassword,
-  database: 'Splitmate',
-  waitForConnections: true,
-  connectionLimit: 20, 
-  queueLimit: 0, // 等待連線請求數，0 表示不限制
-  connectTimeout: 10000, // 10秒超時
-  idleTimeout: 60000 // 60秒空閒超時
-});
+function createPool() {
+  return mysql.createPool({
+    host: dbHost,
+    user: dbUser,
+    password: dbPassword,
+    database: 'Splitmate',
+    waitForConnections: true,
+    connectionLimit: 50,
+    queueLimit: 0,
+    connectTimeout: 10000, // 10秒超時
+    idleTimeout: 60000 // 60秒空閒超時
+  });
+}
 
-// 使用連接池查詢
+function resetPool() {
+  pool.end(); // 關閉現有連接池
+  pool = createPool(); // 重新創建連接池
+  console.log('Connection pool has been reset.');
+}
+
 export class Database {
-  static async executeQuery(query, params = []) {
+  static async executeQuery(query, params = [], retryCount = 2) {
     let connection;
-    try {
-      connection = await pool.getConnection();
-      const [results] = await connection.execute(query, params); //使用解構賦值後，results 會被賦值為第一個元素
+    for (let retry = 1; retry <= retryCount; retry++) {
+      try {
+        connection = await pool.getConnection();
+        const [results] = await connection.execute(query, params);
 
-      // SELECT 查詢
-      if (query.trim().toLowerCase().startsWith("select")) {
-        return results;
-      } else {
-        // 非 SELECT 查詢
-        return {
-          affectedRows: results.affectedRows,
-          insertId: results.insertId
-        };
+        if (query.trim().toLowerCase().startsWith("select")) {
+          return results;
+        } else {
+          return {
+            affectedRows: results.affectedRows,
+            insertId: results.insertId
+          };
+        }
+      } catch (error) {
+        console.error(`Retry ${retry} - Error while connecting to MySQL:`, error);
+
+        if (retry === retryCount) {
+          // 當重試次數達到上限時，重新初始化連接池
+          resetPool();
+          throw error;
+        }
+
+        // 在下一次重試之前，等待一段時間（比如 1 秒）
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      } finally {
+        if (connection) connection.release();
       }
-    } catch (error) {
-      console.error('Error while connecting to MySQL', error);
-      throw error;
-    } finally {
-      if (connection) connection.release();
     }
   }
 }
