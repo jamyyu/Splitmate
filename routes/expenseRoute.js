c1e8ca438bee33b5b81e2621bb45b283303e8583
jamyyu <jamy11212@gmail.com>
2024-08-16 14:48:07 +0800

    Set up routes for expense exchange rate retrieval and data upload

---
+++ routes/expenseRoute.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import multer from 'multer';
+import { getExchangeRate, uploadExpenseData } from '../controllers/expenseController.js';
+
+const router = express.Router();
+
+// 設置 multer 的存儲方式為內存存儲
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+router.get('/api/expense/exchange-rate', getExchangeRate);
+
+router.post('/api/expense',upload.single('image'), uploadExpenseData)
+
+
+
+export default router;
