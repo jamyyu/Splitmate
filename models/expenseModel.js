77680ef7e084d032b34bd4ff0b19a6ea158db97a
jamyyu <jamy11212@gmail.com>
2024-08-17 17:41:41 +0800

    Add getExpense function to retrieve expense data from the database

--- models/expenseModel.js
+++ models/expenseModel.js
@@ -7,3 +7,33 @@ export const createExpense = async (groupId, date, time, category, item, currenc
   console.log('addExpense', results);
   return results.insertId;
 };
+
+
+export const getExpense = async (groupId) => {
+  const query =`
+  SELECT 
+    e.id AS expense_id,
+    CONVERT_TZ(e.date, '+00:00', '+08:00') AS date,
+    e.time,
+    e.category,
+    e.item,
+    e.currency,
+    e.amount,
+    p.payer,
+    p.amount AS paid_amount,
+    sm.member,
+    sm.amount AS member_amount
+  FROM 
+    expense e
+  JOIN 
+    payer p ON e.id = p.expense_id
+  JOIN 
+    SplitMember sm ON e.id = sm.expense_id
+  WHERE 
+    e.splitgroup_id = ?
+  ORDER BY 
+    e.date DESC,
+    e.time DESC;`
+  const results = await Database.executeQuery(query, [groupId]);
+  return results;
+};
