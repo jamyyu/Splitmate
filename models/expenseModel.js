fd0a5676d0c5f031bfca33c08856d3769e94def8
jamyyu <jamy11212@gmail.com>
2024-08-18 19:23:18 +0800

    Add function to calculate balance

--- models/expenseModel.js
+++ models/expenseModel.js
@@ -4,7 +4,6 @@ import { Database } from '../dbconfig.js';
 export const createExpense = async (groupId, date, time, category, item, currency, amount, exchangeRate, mainCurrencyAmount, note, image_name) => {
   const query = 'INSERT INTO expense (splitgroup_id, date, time, category, item, currency, amount, exchangeRate, mainCurrencyAmount, note, image_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
   const results = await Database.executeQuery(query, [groupId, date, time, category, item, currency, amount, exchangeRate, mainCurrencyAmount, note, image_name]);
-  console.log('addExpense', results);
   return results.insertId;
 };
 
@@ -35,5 +34,61 @@ export const getExpense = async (groupId) => {
     e.date DESC,
     e.time DESC;`
   const results = await Database.executeQuery(query, [groupId]);
+  return results;
+};
+
+
+export const getGroupBalance = async (groupId) => {
+  const query = `
+    WITH AmountPaid AS (
+      SELECT 
+        p.payer AS member,
+        SUM(p.mainCurrencyAmount) AS totalAmountPaid,
+        e.splitgroup_id
+      FROM 
+        payer p
+      JOIN 
+         expense e ON p.expense_id = e.id
+      WHERE 
+        e.splitgroup_id = ?
+      GROUP BY 
+        p.payer, e.splitgroup_id
+    ),
+    AmountToPay AS (
+      SELECT 
+        sm.member,
+        SUM(sm.mainCurrencyAmount) AS totalAmountToPay,
+        e.splitgroup_id
+      FROM 
+        SplitMember sm
+      JOIN 
+        expense e ON sm.expense_id = e.id
+      WHERE 
+        e.splitgroup_id = ?
+      GROUP BY 
+        sm.member, e.splitgroup_id
+    )
+    SELECT 
+        COALESCE(AP.member, ATP.member) AS member,
+        COALESCE(AP.totalAmountPaid, 0) AS totalAmountPaid,
+        COALESCE(ATP.totalAmountToPay, 0) AS totalAmountToPay,
+        COALESCE(AP.totalAmountPaid, 0) - COALESCE(ATP.totalAmountToPay, 0) AS balance
+    FROM 
+        AmountPaid AP
+    LEFT JOIN 
+        AmountToPay ATP ON AP.member = ATP.member AND AP.splitgroup_id = ATP.splitgroup_id
+    UNION
+    SELECT 
+        COALESCE(AP.member, ATP.member) AS member,
+        COALESCE(AP.totalAmountPaid, 0) AS totalAmountPaid,
+        COALESCE(ATP.totalAmountToPay, 0) AS totalAmountToPay,
+        COALESCE(AP.totalAmountPaid, 0) - COALESCE(ATP.totalAmountToPay, 0) AS balance
+    FROM 
+        AmountToPay ATP
+    LEFT JOIN 
+        AmountPaid AP ON ATP.member = AP.member AND ATP.splitgroup_id = AP.splitgroup_id;
+  `;
+  const results = await Database.executeQuery(query, [groupId,groupId]);
+  console.log('getGroupBalances', results);
   return results;
 };
