f95463cfb5b4bbc7e35cc7b669415d82b7e6ec7f
jamyyu <jamy11212@gmail.com>
2024-08-27 16:10:39 +0800

    Add update and delete group functionalities

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,9 +1,10 @@
 import 'dotenv/config';
-import { uploadFileToS3 } from '../services/S3.js';
-import { createGroup } from '../models/groupModel.js';
+import { Database } from '../dbconfig.js';
+import { uploadFileToS3, deleteFilefromS3 } from '../services/S3.js';
+import { createGroup, updateGroupById, getGroupDataById, deleteGroupById } from '../models/groupModel.js';
 import { createMember, getMemberByEmail } from '../models/memberModel.js';
 import { getUserByEmail } from '../models/userModel.js';
-import { addMemberToGroup, getGroupByUserId, getMemberByGroupId} from '../models/groupMemberModel.js';
+import { addMemberToGroup, getGroupByUserId, getMemberByGroupId, removeMemberFromGroup, UpdateMemberInGroup } from '../models/groupMemberModel.js';
 import validator from 'validator';
 import jwt from 'jsonwebtoken';
 
@@ -27,7 +28,6 @@ const secretKey = process.env.JWT_SECRET_KEY;
 export const uploadGroupData = async (req, res) => {
   // 會員驗證
   const token = req.headers.authorization?.split(' ')[1];
-  console.log(token);
   try {
     const payload = jwt.verify(token, secretKey, { algorithms: ['HS256'] });
   } catch (error) {
@@ -43,10 +43,8 @@ export const uploadGroupData = async (req, res) => {
         return res.status(500).json({ 'message': 'Image upload failed', 'details': data });
       }
     }
-    // 將 req.body 轉換為普通
+    // 將 req.body 轉換為普通物件
     const plainBody = convertToPlainObject(req.body);
-    console.log(plainBody);
-    console.log(imageName);
     // 驗證成員電子郵件
     for (let member of plainBody.members) {
       if (!validator.isEmail(member.email)) {
@@ -82,7 +80,6 @@ export const uploadGroupData = async (req, res) => {
       // 將成員加入群組
       await addMemberToGroup(groupId, memberId, memberName, userId, role);
     }));
-
     res.status(200).json({ ok: true });
   } catch (error) {
     console.error('Error saving members or adding them to the group:', error);
@@ -126,10 +123,8 @@ export const getGroupsData = async (req, res) => {
 export const getGroupData = async (req, res) => {
   // 會員驗證
   const token = req.headers.authorization?.split(' ')[1];
-  let userId;
   try {
     const payload = jwt.verify(token, secretKey, { algorithms: ['HS256'] });
-    userId = payload.id;
   } catch (error) {
     console.error('JWT 驗證失敗:', error);
     return res.status(403).json({ error: true, message: 'Not logged in, access denied' });
@@ -152,6 +147,7 @@ export const getGroupData = async (req, res) => {
     const memberData = results.map(result => ({
       member_id: result.member_id,
       member_name: result.member_name,
+      member_email: result.member_email,
       role: result.role,
       user_id: result.user_id,
       user_name: result.user_name
@@ -162,3 +158,146 @@ export const getGroupData = async (req, res) => {
   }
 };
 
+
+export const updateGroupData = async(req, res) => {
+  //let connection;
+  // 會員驗證
+  const token = req.headers.authorization?.split(' ')[1];
+  let userId;
+  try {
+    const payload = jwt.verify(token, secretKey, { algorithms: ['HS256'] });
+    userId = payload.id;
+  } catch (error) {
+    console.error('JWT 驗證失敗:', error);
+    return res.status(403).json({ error: true, message: 'Not logged in, access denied' });
+  }
+  try {
+    // 獲取路由中的 groupId 參數
+    const groupId = req.params.groupId;
+    const results = await getMemberByGroupId(groupId);
+    if (results.length === 0) {
+      return res.status(404).json({ error: true, message: 'Group not found' });
+    }
+    // 提取原本群組的基本資料
+    const currentGroupData = {
+      id: results[0].splitgroup_id,
+      name: results[0].group_name,
+      main_currency: results[0].main_currency,
+      image_name: results[0].group_image_name,
+    };
+    // 提取原有成員資料
+    const currentMemberData = results.map(result => ({
+      member_id: result.member_id,
+      member_name: result.member_name,
+      member_email: result.member_email,
+      role: result.role,
+      user_id: result.user_id,
+      user_name: result.user_name
+    }));
+    // 將 req.body 轉換為普通物件
+    const plainBody = convertToPlainObject(req.body);
+    console.log(plainBody);
+    // 驗證成員電子郵件
+    for (let member of plainBody.members) {
+      if (!validator.isEmail(member.email)) {
+        return res.status(400).json({ error: true, message: `${member.name} 的電子郵件格式錯誤` });
+      }
+    }
+    // 開始事務
+    //connection = await Database.beginTransaction();
+    // 圖片更新
+    let oldImageName = currentGroupData.image_name;
+    let newImageName = oldImageName;
+    if (req.file) {
+      if (oldImageName) {
+        await deleteFilefromS3(oldImageName); 
+      }
+      const data = await uploadFileToS3(req.file);
+      newImageName = data.key;
+      if (data.$metadata.httpStatusCode !== 200) {
+        return res.status(500).json({ 'message': 'Image upload failed', 'details': data });
+      }
+    }
+    // 群組資料更新
+    await updateGroupById(plainBody.groupName, newImageName, groupId);
+    // 成員資料更新
+    const newMemberIds = new Set(plainBody.members.map(m => m.memberId));
+    const membersToRemove = currentMemberData.filter(m => !newMemberIds.has(m.member_id.toString()));
+    await Promise.all(membersToRemove.map(async (member) => {
+      await removeMemberFromGroup(groupId, member.member_id); // 從群組刪除成員
+    }));
+    const memberResults = await Promise.all(plainBody.members.map(async member => {
+      let { memberId, name, email } = member;
+      // 將 'null' 或空字符串處理為 null
+      if (memberId === 'null' || !memberId) {
+        memberId = null;
+      }
+      // 原有群組成員更新姓名
+      if (memberId){
+        await UpdateMemberInGroup(name, groupId, memberId);
+        return null;
+      } else {
+        // 添加新成員
+        let memberData = await getMemberByEmail(email);
+        if (!memberData) {
+          // 如果這個email尚未註冊過，則創建新成員
+          memberId = await createMember(email);
+        } else {
+          // 如果這個email已經註冊過，則使用現有的成員ID
+          memberId = memberData.id;
+        }
+        return { memberId, email, name };
+      }
+    }));
+    // 過濾掉 null 的結果
+    const filteredMemberResults = memberResults.filter(result => result !== null);
+    if (filteredMemberResults.length > 0) {
+      // 資料存入群組成員關聯資料庫
+      await Promise.all(filteredMemberResults.map(async ({ memberId, email, name }) => {
+        let userId = null;
+        const userResults = await getUserByEmail(email);
+        if (userResults) {
+          userId = userResults.id;
+        }
+        const role = 'member';
+        // 將成員加入群組
+        await addMemberToGroup(groupId, memberId, name, userId, role);
+      }));
+    }
+    //await connection.commit(); // 提交事務
+    res.status(200).json({ ok: true });
+  } catch (error) {
+    //if (connection) {
+      //await connection.rollback(); // 回滾事務並確保 connection 已定義
+    //}
+    console.error('更新群組資料時出錯:', error);
+    res.status(500).json({ error: true, message: 'Internal Server Error' });
+  }
+}
+
+
+export const deleteGroupData = async(req, res) => {
+  // 會員驗證
+  const token = req.headers.authorization?.split(' ')[1];
+  try {
+    const payload = jwt.verify(token, secretKey, { algorithms: ['HS256'] });
+  } catch (error) {
+    return res.status(403).json({ error: true, message: 'Not logged in, access denied' });
+  }
+  try {
+    const groupId = req.params.groupId;
+    const results = await getGroupDataById(groupId);
+    if (results === null) {
+      return res.status(404).json({ error: true, message: 'Group not found' });
+    }
+    // 刪除S3的群組圖片
+    let oldImageName = results.image_name;
+    if (oldImageName) {
+      await deleteFilefromS3(oldImageName); 
+    }
+    await deleteGroupById(groupId);
+    res.status(200).json({ ok: true });
+  } catch (error) {
+    return res.status(400).json({ error: true, message: error.message });
+  }
+}
