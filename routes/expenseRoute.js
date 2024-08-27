03fe8cb743486a25dab759ff6705b9d82809b6fa
jamyyu <jamy11212@gmail.com>
2024-08-27 16:24:38 +0800

    Update expense and group routes for enhanced functionality

--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import { getExchangeRate, uploadExpenseData, getExpenseData, getGroupBalanceData } from '../controllers/expenseController.js';
+import { getExchangeRate, uploadExpenseData, getAllRecordData, getGroupBalanceData, getExpenseData, deleteExpenseData } from '../controllers/expenseController.js';
 
 const router = express.Router();
 
@@ -13,8 +13,14 @@ router.get('/api/expense/exchange-rate', getExchangeRate);
 
 router.post('/api/expense',upload.single('image'), uploadExpenseData)
 
-router.get('/api/expense/:groupId', getExpenseData)
-
 router.get('/api/expense/:groupId/balance', getGroupBalanceData)
+
+router.get('/api/expense/:groupId/:expenseId', getExpenseData)
+
+router.delete('/api/expense/:groupId/:expenseId', deleteExpenseData)
+
+router.get('/api/expense/:groupId', getAllRecordData)
+
+
 
 export default router;
